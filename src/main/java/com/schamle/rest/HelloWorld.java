55f225638691f6e6b657a85d1e6148b7d401b0d0
Marc Schamle <mschamle@gmail.com>
2021-05-16 14:44:48 +0200

    Add second endpoint to return dynamic result

--- src/main/java/com/schamle/rest/HelloWorld.java
+++ src/main/java/com/schamle/rest/HelloWorld.java
@@ -2,11 +2,19 @@ package com.schamle.rest;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
 
 @Path("hello")
 public class HelloWorld {
     @GET
-    public String get() {
-        return "Hello World";
+    public Response get() {
+        return Response.status(200).entity("Hello World").build();
+    }
+
+    @GET
+    @Path("{name}")
+    public Response getName(@PathParam("name") String name) {
+        return Response.status(200).entity("Hello " + name).build();
     }
 }
